8bef8d10cda3ad5e0e51ed04b4200a6a1fe3edf6
Claude Weidner <cwaffiliateinvestments@gmail.com>
2025-11-03 12:52:59 -0600

    Remove unused formatCurrency import

--- client/src/pages/SoldProperties.js
+++ client/src/pages/SoldProperties.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { formatCurrency } from '../api';
 
 function SoldProperties() {
   const [properties, setProperties] = useState([]);
